654b3caa79eb9ebbf30767924ba28f3d3d84709d
Raappsstunus <136825908+Raappsstunus@users.noreply.github.com>
2023-06-16 23:00:15 +0700

    sarap

--- index.py
+++ index.py
@@ -1,8 +1,3 @@
 print('ilman push');
-print('ilman nurahman');
-<<<<<<< HEAD
-print("abcd")
-print("asada")
-=======
-print("anggun");
->>>>>>> 4b32ec2138f790d2eb8a2e6f044be4a0aff8c63b
+print('ilman nurahman')
+print("mosu")
